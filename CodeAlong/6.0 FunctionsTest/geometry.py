019c76b9cc5256639ad4737a86385dc8b7d27498
FabianAndersson17 <89392496+FabianAndersson17@users.noreply.github.com>
2021-09-07 10:44:57 +0200

    Functions class done. Tested importing of a script to a script

---
+++ CodeAlong/6.0 FunctionsTest/geometry.py
@@ -0,0 +1,8 @@
+def triangle_area(base, height):
+    return base*height/2
+
+def triangle_curcumference(side1, side2, side3):
+    return sum(side1, side2, side3)
+
+def square_area(side):
+    return side**2
---
+++ CodeAlong/6.0 FunctionsTest/main.py
@@ -0,0 +1,6 @@
+import geometry as geo
+
+triangle1_area = geo.triangle_area(5,8)
+print(triangle1_area)
+
+print(geo.square_area(10))

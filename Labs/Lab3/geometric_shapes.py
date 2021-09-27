d8030a37f7772d725f597fc4eeb680c61fdaf58e
FabianAndersson17 <89392496+FabianAndersson17@users.noreply.github.com>
2021-09-27 14:49:32 +0200

    Added Error handling. Added area calulator and circumference calulator. Added a repr. And prepared __eq__, translate ans is_inside for later use.

---
+++ Labs/Lab3/geometric_shapes.py
@@ -0,0 +1,57 @@
+import math
+
+class Geometric_Shapes():
+    def __init__(self, xPoint: float, yPoint: float, radius: float) -> None:
+        self.xPoint = xPoint
+        self.yPoint = yPoint
+        self.radius = radius
+        self.diameter = radius*2
+        if not isinstance(self.xPoint, (float, int)):
+            raise ValueError(f"xPoint needs to be a float or an int not {type(self.xPoint)}")
+        if not isinstance(self.yPoint, (float, int)):
+            raise ValueError(f"yPoint needs to be a float or an int not {type(self.yPoint)}")
+        if not isinstance(self.radius, (float, int)):
+            raise ValueError(f"radius needs to be a float or an int not {type(self.radius)}")
+            
+        try:
+            self.xPoint, self.yPoint, self.radius
+        except ValueError as err:
+            print(err)
+
+    def area_calulator(self) -> float:
+        return(self.diameter * self.diameter)
+    
+    def circumference_calulator(self) -> float:
+        return self.diameter*4
+
+    def __eq__(self, o: object) -> bool:
+        pass
+
+    def translate(self) -> float:
+        pass
+
+    def is_inside_point(self) -> True:
+        pass
+
+    def __repr__(self) -> str:
+        return (f"x-coordinate = {self.xPoint}, y-coordinate = {self.yPoint}, radius = {self.radius}, diameter = {self.diameter}")
+
+class Square(Geometric_Shapes):
+    def __init__(self, xPoint, yPoint, radius, height) -> None:
+        super().__init__(xPoint, yPoint, radius)
+        self.height = height
+
+    def area_calulator(self) -> float:
+        return self.diameter * self.height
+
+    def circumference_calulator(self) -> float:
+        return self.diameter*2 + self.height*2
+
+    def translate(self) -> float:
+        return super().translate()
+    
+    def is_inside_point(self) -> True:
+        return super().is_inside_point()
+
+    def __repr__(self) -> str:
+        return (f"x-coordinate = {self.xPoint}, y-coordinate = {self.yPoint}, height = {self.height}, widht = {self.diameter}")

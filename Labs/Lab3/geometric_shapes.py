2d13d3588ecf88a2a58afbb2af815a308c85d18e
FabianAndersson17 <89392496+FabianAndersson17@users.noreply.github.com>
2021-09-28 11:42:41 +0200

    Started working on fixing with checkig if 2 shapes are the same size

--- Labs/Lab3/geometric_shapes.py
+++ Labs/Lab3/geometric_shapes.py
@@ -19,9 +19,11 @@ class Geometric_Shapes():
     def circumference_calulator(self) -> float:
         return self.diameter*4
 
-    def __eq__(self, o: object) -> bool:
-        pass
-
+    def __eq__(self, other) -> bool:
+        if other == self and other == self.area_calulator and other == self.circumference_calulator:
+            return "The shapes are the same size"
+        else:
+            return "The shapes are not the same size"
     def translate(self) -> float:
         pass
 

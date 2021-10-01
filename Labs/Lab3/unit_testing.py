72e2d14f6772a47d4e4053fbcc4f0c4630ee4141
FabianAndersson17 <89392496+FabianAndersson17@users.noreply.github.com>
2021-10-01 11:44:58 +0200

    Started adding test to unit testing

--- Labs/Lab3/unit_testing.py
+++ Labs/Lab3/unit_testing.py
@@ -1,3 +1,16 @@
 import unittest
 from geometric_shapes import Rectangle
 
+class TestRectangle(unittest.TestCase):
+    def setUp(self) -> None:
+        self.x, self.y, self.width, self.height = 0, 0, 3, 4
+
+    def creat_Rectangle(self) -> "Rectangle":
+        return Rectangle(self.x, self.y, self.width, self.height)
+
+    ## Testing starts here - all tests must start with rectangle_test_
+
+    ## Test: creat rectangles
+
+    def rectangle_test_creat(self) -> None:
+        pass

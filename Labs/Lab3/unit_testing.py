2f3cf21a733310c2782e20c15c977d8e106625e5
FabianAndersson17 <89392496+FabianAndersson17@users.noreply.github.com>
2021-10-01 14:28:34 +0200

    Continued working on unit testing

--- Labs/Lab3/unit_testing.py
+++ Labs/Lab3/unit_testing.py
@@ -1,16 +1,25 @@
-import unittest
 from geometric_shapes import Rectangle
+import unittest
 
 class TestRectangle(unittest.TestCase):
     def setUp(self) -> None:
         self.x, self.y, self.width, self.height = 0, 0, 3, 4
 
     def creat_Rectangle(self) -> "Rectangle":
-        return Rectangle(self.x, self.y, self.width, self.height)
+        return Rectangle(self.xPoint, self.yPoint, self.width, self.height)
 
-    ## Testing starts here - all tests must start with rectangle_test_
+    ## Testing starts here - all tests must start with test_
 
     ## Test: creat rectangles
 
-    def rectangle_test_creat(self) -> None:
-        pass
+    def test_creat_rectangle(self) -> None:
+        rec = self.creat_Rectangle()
+        self.assertEqual(rec(self.xPoint, self.yPoint, self.width, self.height), (1, 2, 3, 4))
+
+    def test_empty_rectangle(self):
+        with self.assertEqual(ValueError):
+            rec = Rectangle()
+    
+    def test_creat_invalid_rectangle(self):
+        with self.assertEqual(TypeError):
+            rec = Rectangle(0, 0, "Two", 5)
